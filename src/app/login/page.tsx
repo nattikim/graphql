e2347041029bcd8c7ee4b6e9fa4147e47567a92c
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-07-27 01:30:06 +0300

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,44 @@
+export default function Login() {
+  return (
+    <div className="z-10 m-auto">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login</h1>
+          <p className="py-6">Check your Grit:lab progress.</p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl backdrop-blur-3xl">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
