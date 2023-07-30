1e0631856ed7b65f4e3d1b112cad12b366c63a6f
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-07-31 02:09:25 +0300

    add Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="z-50 flex flex-col items-center opacity-30 text-info">
+      <span className="loading loading-spinner m-auto w-[43%]"></span>
+    </div>
+  );
+};
+
+export default Loading;
