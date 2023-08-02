b0c85933246de1ea137a2278331b5195b197d754
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-08-03 02:21:03 +0300

    add gritlab logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,15 @@
 import ButtonLogout from "./ButtonLogout";
+import gritlabLogo from "../../public/gritlab.png";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className={"z-50 fixed flex w-full justify-end p-5"}>
+    <nav className={"z-50 fixed flex w-full justify-between p-5 items-center"}>
+      <Image
+        src={gritlabLogo}
+        alt="gritlab logo"
+        className={"w-24 h-7 brightness-50 dark:brightness-75"}
+      />
       <ButtonLogout />
     </nav>
   );
