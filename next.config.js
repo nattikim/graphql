98da52b206b7dcc9316b982fc7bc81e8e220469d
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-08-11 17:38:22 +0300

    configure image remote pattern

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "01.gritlab.ax",
+        port: "",
+        pathname: "/git/user/avatar/**",
+      },
+    ],
     domains: ["01.gritlab.ax"],
   },
 };
