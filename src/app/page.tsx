821721a64e85f84da15a76b1967bcae112e736d6
Natalia Kim <93912586+nattikim@users.noreply.github.com>
2023-08-11 16:32:27 +0300

    add components
    fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,109 @@
-import Image from "next/image";
+"use client";
 
-export default async function Home() {
+import Loading from "@/components/Loading";
+import { useInfo } from "@/hooks/info";
+import { useEffect } from "react";
+import { getUserId } from "@/lib/userId";
+import UserCard from "@/components/UserCard";
+import { useRouter } from "next/navigation";
+import Skills from "@/components/Skills";
+import { useProgress } from "@/hooks/charts";
+import Charts from "@/components/Charts";
+
+export default function Home() {
+  const router = useRouter();
+
+  let jwtToken = null;
+  if (typeof window !== "undefined") {
+    jwtToken = localStorage.getItem("jwtToken");
+  }
+  const userId = getUserId(jwtToken);
+
+  useEffect(() => {
+    if (userId === undefined) {
+      router.push("/login");
+    }
+  }, [userId, router]);
+
+  const progress = useProgress(userId!);
+  const { user, loading, error } = useInfo(userId!);
+
+  const roundedAuditRatio = Math.round(user.auditRatio * 10) / 10;
+
+  if (loading) return <Loading />;
+
+  if (error)
+    return (
+      <p
+        className={
+          "z-10 p-5 bg-base-100 text-xl rounded-md text-error text-center"
+        }
+      >
+        Error: {JSON.stringify(error.message)}
+      </p>
+    );
+
   return (
-    <main className="z-10 flex flex-col items-center">
+    <main className="z-10 items-center mt-7 text-center sm:text-left">
       <div className="flex flex-col sm:grid sm:grid-cols-2 gap-5 ">
-        <div className="flex flex-col items-center justify-between w-full">
-          <div className="card card-side backdrop-blur-3xl shadow-xl">
-            <figure
-              className={"w-32 h-32 rounded-full bg-primary m-5 self-center"}
-            >
-              <Image
-                src={""}
-                alt="Avatar"
-                className={"w-32 h-32 rounded-full bg-primary m-5 self-center"}
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">UPS AND DOWNS</h2>
-              <p>UPS AND DOWNS</p>
+        <div className="flex flex-col items-center justify-between w-full h-full gap-5">
+          <UserCard userId={userId} />
+          <div
+            className={
+              "flex flex-col lg:flex-row items-center justify-between text-center w-full h-full gap-5"
+            }
+          >
+            <div className="card backdrop-blur-3xl shadow-xl w-full h-full ">
+              <div className="card-body">
+                TOTAL XP
+                <p
+                  className={
+                    "font-black text-xl font-mono text-neutral flex flex-col justify-center"
+                  }
+                >
+                  {user.xpAmount.aggregate.sum.amount}
+                </p>
+              </div>
+            </div>
+
+            <div className="card backdrop-blur-3xl shadow-xl w-full h-full">
+              <div className="card-body">
+                LEVEL
+                <p
+                  className={
+                    "font-black text-xl font-mono text-neutral flex flex-col justify-center"
+                  }
+                >
+                  {user.transactions[0].amount}
+                </p>
+              </div>
+            </div>
+            <div className="card backdrop-blur-3xl shadow-xl w-full h-full">
+              <div className="card-body">
+                RATIO
+                <p
+                  className={
+                    "font-black text-xl font-mono text-neutral flex flex-col justify-center"
+                  }
+                >
+                  {roundedAuditRatio}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-between w-full">
+        <div className="flex flex-col justify-between w-full">
           <div className="card card-side w-full backdrop-blur-3xl shadow-xl min-h-full">
             <div className="card-body">
-              <h2 className="card-title">UPS AND DOWNS</h2>
-              <p>UPS AND DOWNS</p>
+              <h2 className="card-title justify-center mb-3">SKILLS</h2>
+              <Skills userId={userId} />
             </div>
           </div>
         </div>
         <div className="col-span-2 w-full m-auto flex flex-col items-center justify-between">
           <div className="card card-side w-full backdrop-blur-3xl shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">UPS AND DOWNS</h2>
-              <p>UPS AND DOWNS</p>
+            <div className="card-body items-center">
+              <Charts userId={userId} />
             </div>
           </div>
         </div>
